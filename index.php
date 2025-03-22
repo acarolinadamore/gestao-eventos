79cb44b2d14d03f73858c11b6a519fd1b34afef8
Ana <acarolinadamore@gmail.com>
2025-03-21 20:25:23 -0400

    Index e imagens

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Eventos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="global.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark index-navbar">
+        <div class="container">
+            <div class="logo-container">
+            <a class="navbar-brand" href="index.php">Evenza</a>                        
+            </div>
+            <div class="navbar-btn-container">
+                <button class="btn btn-primary">Entrar</button>
+            </div>            
+        </div>
+    </nav>
+    <div class="container mt-5">
+        <h2>Evenza - Gestão de Eventos</h2>        
+    </div>
+    <div class="container mt-5">
+        <p>Bem-vindo ao Evenza, a solução desenvolvida para facilitar a gestão de eventos corporativos, reuniões estratégicas, webaulas e confraternizações.</p>
+        <p>Nossa plataforma foi criada para otimizar a experiência dos organizadores e garantir o controle completo da sua programação, do convite ao RSPV, passando por confirmações personalizadas e relatórios detalhados. </p>
+    </div>
+    <div class="container mt-5">
+        <h2>Por que escolher o Evenza?</h2>
+        <p>Soluções sob medida para cada evento</p>
+        <p>Convites e RSVP personalizados</p>
+        <p>Painel intuitivo e gestão centralizada</p>
+        <p>Automação e notificações inteligentes</p>
+        <p>Segurança e confiabilidade</p>
+    </div>
+    <div class="container mt-5">
+        <h2>Funcionalidades Principais</h2>
+        <p>Modelos de Landing Page</p>
+        <p>Gerenciamento de Participantes</p>
+        <p>RSVP Inteligente</p>
+        <p>Relatórios e Métricas</p>
+        <p>Notificações e E-mails automáticos</p>
+    </div>
+   <div class="container mt-5">
+        <h2>Para quem é o Evenza?</h2>
+        <p>Empresas que organizam eventos corporativos e reuniões estratégicas.</p>
+        <p>Departamentos de Marketing e comunicação que realizam webaulas e treinamentos</p>        
+        <p>Equipes que precisam gerenciar confirmações de presença e preferencia de participantes</p>
+        <p>Organizadores de confraternizações, festas de fim de ano, eventos internos e externos.</p>
+    </div>
+    <div class="container mt-5">
+        <h2>Comece agora!</h2>
+        <p>Facilite a gestão dos seus eventos com uma plataforma que oferece eficiencia, controle e praticidade.</p>
+        <p>Solicite uma demonstração ou entre em contato para saber mais.</p>
+    </div>
+    <div class="container mt-5">
+        <h2>Agende um teste gratuito</h2>
+        <p>Fale Conosco</p>
+        <p>Agende um teste gratuito</p>
+    </div>
+</body>
+</html>
